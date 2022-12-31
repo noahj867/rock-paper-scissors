a1138ccac06b2a65a7de02ec7f3763809c2df970
Noah Solnick <solnick.noah@gmail.com>
2022-12-31 12:24:02 -0600

    Finish 5 round rock paper scissors game that keeps score and prints who won

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -4,71 +4,93 @@ let rock = "rock"
 let paper = "paper"
 let scissors = "scissors"
 
-let userChoice = prompt("Please select rock, paper, or scissors");
-console.log(userChoice)
+let tie = 0 
+let win = 0 
+let lose = 0
 
-// Computer also has same three choices, determined by a randomized number from 0-2
+// Computer also has three choices, determined by a randomized number from 0-2
 let options = [rock,paper,scissors];
-
-let getComputerChoice = Math.floor(Math.random() * options.length);
-console.log(getComputerChoice)
 
 // Convert randomized number to rock, paper, or scissors
 
 function convertComputerChoice(getComputerChoice){
     if (getComputerChoice === 0){
-        // console.log(rock)
         return rock;
     } else if (getComputerChoice === 1){
-        // console.log(paper)
         return paper;
     } else {
-        // console.log(scissors)
         return scissors;
     }
 }
-convertComputerChoice(getComputerChoice)
-
-// Store computer's choice as a variable
-
-let computerChoice = convertComputerChoice(getComputerChoice)
-
 // Pass user's and computer's choices as inputs through a function that determines the outcome of the game
-
+function playRound () {
+    let userChoice = prompt("Please select rock, paper, or scissors")
+    console.log(userChoice)
+    let getComputerChoice = Math.floor(Math.random() * options.length);
+    let computerChoice = convertComputerChoice(getComputerChoice)
+    console.log(computerChoice)
 // If user chooses rock
-function playRound(userChoice, computerChoice){
     if (userChoice.toLowerCase() === "rock" && computerChoice === rock) {
-        console.log("it's a tie! Rock ties with Rock")
-        return "It's a tie! Rock vs Rock";
+        console.log("it's a tie! Rock vs Rock")
+        return "tie";
     } else if (userChoice.toLowerCase() === "rock" && computerChoice === paper) {
         console.log("You lose! Paper beats rock")
-        return "You lose :(. Paper beats rock";
+        return "loss";
     } else if (userChoice.toLowerCase() === "rock" && computerChoice === scissors) {
         console.log("You win! Rock beats Scissors")
-        return "oops haven't gotten there yet"; 
+        return "win"; 
 
 // If user chooses paper
     } else if (userChoice.toLowerCase() === "paper" && computerChoice === rock) {
         console.log("You win! Paper beats rock")
-        return "You win! Paper beats rock";
+        return "win";
     } else if (userChoice.toLowerCase() === "paper" && computerChoice === paper) {
         console.log("It's a tie! Paper ties paper")
-        return "It's a tie! Paper vs paper";
+        return "tie";
     } else if (userChoice.toLowerCase() === "paper" && computerChoice === scissors) {
         console.log("You lose! Scissors beats paper")
-        return "You lose! Scissors beats paper";
+        return "loss";
 
 // If user chooses scissors
     } else if (userChoice.toLowerCase() === "scissors" && computerChoice === rock) {
         console.log("You lose! Rock beats scissors")
-        return "You lose! Rock beats scissors";
+        return "loss";
     } else if (userChoice.toLowerCase() === "scissors" && computerChoice === paper) {
         console.log("You win! Scissors beats paper")
-        return "You win! Scissors beats paper";
+        return "win";
     } else (userChoice.toLowerCase() === "scissors" && computerChoice === scissors)
         console.log("It's a tie! Scissors ties Scissors")
-        return "It's a tie! Scissors vs scissors";
+        return "tie";
 }
-playRound(userChoice, computerChoice)
 
 // Play a 5 round game that keeps score and reports winner 
+
+function game() {
+    for (i=0; i<5; i++){
+        let roundResult = playRound()
+        console.log(roundResult)
+        if (roundResult === "tie"){
+            tie++;
+        } else if (roundResult === "win"){
+            win++;
+        } else {
+            lose++ 
+        }
+        console.log("Game score = ", win, "|", lose, "|", tie)
+    }
+    printResult()
+} 
+game()
+
+function printResult() {
+    if (win > lose ) {
+        console.log("Congrats! You win bud")
+    } 
+    else if (lose > win) {
+        console.log("Yikes! You lost pal")
+    }
+    else {
+        console.log("Snoozefest! You tied")
+    }
+}
+
